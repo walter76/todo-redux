1971e62865b4df6526d0621bdec941ee3cd52e05
walter <walter.stocker@gmx.de>
2017-11-17 23:23:20 +0100

    enable filters

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Footer from './Footer'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
     <VisibleTodoList />
+    <Footer />
   </div>
 )
 
