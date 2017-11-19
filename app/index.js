c2d3bbbf364656eaa6fb766116fcb93185dd2a1e
walter <walter.stocker@gmx.de>
2017-11-19 22:14:33 +0100

    used a promise for todos service

--- app/index.js
+++ app/index.js
@@ -8,45 +8,12 @@ import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/app'
 
+import { createTodosService } from './reducers/todos.service'
+
 let store = createStore(todoApp)
 
-function fetchTodosFromServer () {
-  const todosUri =
-    window.location.protocol + '//' +
-    window.location.host +
-    '/api/todos'
-
-  let headers = new window.Headers()
-  headers.append('content-type', 'application/json; charset=utf-8')
-
-  let options = {
-    method: 'GET',
-    headers: headers,
-    cache: 'default'
-  }
-
-  let request = new window.Request(todosUri, options)
-
-  window.fetch(request)
-  .then((response) => {
-    if (!response.ok) {
-      console.log('status: ', response.status)
-      console.log(response.statusText)
-
-      return {
-        success: false,
-        items: []
-      }
-    }
-
-    return response.json()
-  })
-  .then((json) => {
-    console.table(json)
-  })
-}
-
-fetchTodosFromServer()
+let todosService = createTodosService()
+todosService.getAll()
 
 render(
   <Provider store={store}>
---
+++ app/reducers/todos.service.js
@@ -0,0 +1,47 @@
+export function createTodosService () {
+  const todosUri =
+    window.location.protocol + '//' +
+    window.location.host +
+    '/api/todos'
+
+  function getAll () {
+    let headers = new window.Headers()
+    headers.append('content-type', 'application/json; charset=utf-8')
+
+    let options = {
+      'method': 'GET',
+      'headers': headers,
+      'cache': 'default'
+    }
+
+    let request = new window.Request(todosUri, options)
+
+    return new Promise(
+      (resolve, reject) => {
+        window.fetch(request)
+        .then((response) => {
+          if (!response.ok) {
+            console.log('status: ', response.status)
+            console.log(response.statusText)
+
+            reject({
+              'success': false,
+              'items': []
+            })
+          }
+
+          return response.json()
+        })
+        .then((json) => {
+          console.table(json)
+
+          resolve({
+            'success': true,
+            'items': json
+          })})})
+  }
+
+  return {
+    'getAll': getAll
+  }
+}
