1953b5140494bcc08e24d61d9a4422a0665d7bb8
walter <walter.stocker@gmx.de>
2017-11-19 22:00:24 +0100

    prototype for server side communication

--- app/index.js
+++ app/index.js
@@ -10,6 +10,44 @@ import App from './components/app'
 
 let store = createStore(todoApp)
 
+function fetchTodosFromServer () {
+  const todosUri =
+    window.location.protocol + '//' +
+    window.location.host +
+    '/api/todos'
+
+  let headers = new window.Headers()
+  headers.append('content-type', 'application/json; charset=utf-8')
+
+  let options = {
+    method: 'GET',
+    headers: headers,
+    cache: 'default'
+  }
+
+  let request = new window.Request(todosUri, options)
+
+  window.fetch(request)
+  .then((response) => {
+    if (!response.ok) {
+      console.log('status: ', response.status)
+      console.log(response.statusText)
+
+      return {
+        success: false,
+        items: []
+      }
+    }
+
+    return response.json()
+  })
+  .then((json) => {
+    console.table(json)
+  })
+}
+
+fetchTodosFromServer()
+
 render(
   <Provider store={store}>
     <App />
--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
-express()
+var app = express()
+
+var todos = [
+  { 'id': 100, 'text': 'something to do', 'completed': false },
+  { 'id': 101, 'text': 'nothing to do', 'completed': true },
+  { 'id': 102, 'text': 'something else to do', 'completed': false }
+]
+
+app.get('/api/todos', function(req, res, next) {
+  res.send(todos)
+})
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
