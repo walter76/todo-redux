801f3605f84375c8af9585ad710389f37b39199e
walter <walter.stocker@gmx.de>
2017-11-20 23:31:31 +0100

    added mongodb support

--- server/index.js
+++ server/index.js
@@ -1,54 +1,87 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
 const PORT = process.env.PORT || 5000
 
 let app = express()
 
+app.use(express.static(path.join(__dirname, '../public')))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ 'extended': true }))
 
 let nextId = 0
-let todos = []
+let db
 
 app.get('/api/todos', (req, res, next) => {
-  res.send(todos)
+  db.collection('todos').find().toArray((err, results) => {
+    if (err) {
+      return console.log(err)
+    }
+
+    res.send(results.map(todo => {
+      return {
+        'id': todo.id,
+        'text': todo.text,
+        'completed': todo.completed
+      }
+    }))
+  })
 })
 
 app.post('/api/todos', (req, res) => {
-  const todo = {
+  let todo = {
     'id': ++nextId,
     'text': req.body.text,
     'completed': req.body.completed
   }
-  todos.push(todo)
 
-  console.log('new todo added: ' + JSON.stringify(todo))
+  db.collection('todos').insertOne(todo)
+  .then(r => {
+    console.log('saved to database')
+    console.log(JSON.stringify(r.ops))
 
-  res.json(todo)
+    res.json({
+      'id': r.ops[0].id,
+      'text': r.ops[0].text,
+      'completed': r.ops[0].completed
+    })
+  })
 })
 
 app.put('/api/todos', (req, res) => {
-  todos = todos.map(todo => {
-    if (todo.id === req.body.id) {
-      return {
-        'id': todo.id,
-        'text': req.body.text,
-        'completed': req.body.completed
-      }
-    }
-
-    return todo
+  db.collection('todos').updateOne(
+    { 'id': req.body.id },
+    { $set: {
+      'text': req.body.text, 'completed': req.body.completed
+  }})
+  .then(r => {
+    db.collection('todos').findOne({'id': req.body.id})
+    .then(doc => {
+      res.json({
+        'id': doc.id,
+        'text': doc.text,
+        'completed': doc.completed
+      })
+    })
   })
-
-  let updated = todos.find(todo => todo.id === req.body.id)
-
-  console.log('todo updated: ' + JSON.stringify(updated))
-
-  res.json(updated)
 })
 
-app
-  .use(express.static(path.join(__dirname, '../public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+MongoClient.connect('mongodb://localhost:27017/ds-todo-db', (err, database) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  db = database
+
+  // we need a better solution for the ids, especially if we are running multi-
+  // user, but for now this should do it
+  db.collection('todos').find().toArray((err, results) => {
+    results.forEach(todo => {
+      nextId = (todo.id > nextId) ? todo.id : nextId })
+  })
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+})
