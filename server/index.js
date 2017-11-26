923a5ba7d08f1362ded641eceb0de5408fecc678
Walter Stocker <walter.stocker@siemens-healthineers.com>
2017-11-26 16:38:49 +0100

    added a abstract controller registry

--- server/index.js
+++ server/index.js
@@ -16,16 +16,34 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+function ControllerRegistry () {
+  let controllers = []
+
+  function add (controllerCreator) {
+    controllers.push(controllerCreator())
+  }
+
+  function registerRoutes (app) {
+    controllers.forEach(controller => {
+      controller.registerRoutes(app)
+    })
+  }
+
+  return {
+    add: add,
+    registerRoutes: registerRoutes
+  }
+}
+
 MongoClient.connect(MONGODB_URI, (err, database) => {
   if (err) {
     return console.log(err)
   }
 
-  let todoController = createTodoController(database)
-  todoController.registerRoutes(app)
-
-  let userController = createUserController()
-  userController.registerRoutes(app)
+  let controllerRegistry = new ControllerRegistry()
+  controllerRegistry.add(() => createTodoController(database))
+  controllerRegistry.add(createUserController)
+  controllerRegistry.registerRoutes(app)
 
   app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 })
