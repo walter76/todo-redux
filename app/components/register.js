425a4bb573dd9790745b116cd7de805107c23aee
walter <walter.stocker@gmx.de>
2017-12-04 22:59:48 +0100

    basic register form

--- app/components/register.js
+++ app/components/register.js
@@ -1,11 +1,36 @@
+import './register.scss'
+
 import React from 'react'
 
 import MainLayout from './main-layout'
 
 const Register = () => {
+  let username
+  let password
+
   return (
     <MainLayout>
-      <div>Register</div>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+
+        console.log(username.value)
+        console.log(password.value)
+      }}>
+        <input
+          className='register'
+          ref={node => { username = node }}
+          placeholder='Username'
+          autoFocus
+        /><br />
+        <input
+          className='register'
+          ref={node => { password = node }}
+          type='password'
+          placeholder='Password'
+        /><br />
+        <div className='error' />
+        <button className='register' type='submit'>Register</button>
+      </form>
     </MainLayout>
   )
 }
