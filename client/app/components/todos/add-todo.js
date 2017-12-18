0bb6d6099a90c16241693d6334aac4968a2d629b
walter <walter.stocker@gmx.de>
2017-12-18 23:39:26 +0100

    refactoring of add todo to use proper state

--- client/app/components/todos/add-todo.js
+++ client/app/components/todos/add-todo.js
@@ -1,39 +1,70 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { createTodo } from '../../actions/todos'
 import { Form, Button } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
-const AddTodoView = ({ dispatch }) => {
-  let input
+import { createTodo } from '../../actions/todos'
 
-  return (
-    <section>
-      <Form
-        onSubmit={e => {
-          e.preventDefault()
-          if (!input.value.trim()) {
-            return
-          }
-          dispatch(createTodo(input.value))
-          input.value = ''
-        }}
-      >
-        <Form.Field>
-          <input
-            id='todo'
-            name='todo'
-            ref={node => {
-              input = node
-            }}
-            placeholder='My Next ToDo Item'
-          />
-        </Form.Field>
-        <Button primary>Add Todo</Button>
-      </Form>
-    </section>
-  )
+class AddTodo extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      data: {
+        todo: ''
+      }
+    }
+
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+
+    const { todo } = this.state.data
+
+    if (!todo.trim()) {
+      return
+    }
+
+    this.props.dispatch(createTodo(todo.trim()))
+
+    this.setState({
+      data: { todo: '' }
+    })
+  }
+
+  onChange (e) {
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    })
+  }
+
+  render () {
+    const { todo } = this.state.data
+
+    return (
+      <section>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Field>
+            <input
+              id='todo'
+              name='todo'
+              placeholder='My Next ToDo Item'
+              value={todo}
+              onChange={this.onChange}
+            />
+          </Form.Field>
+          <Button primary>Add Todo</Button>
+        </Form>
+      </section>
+    )
+  }
 }
 
-const AddTodo = connect()(AddTodoView)
+AddTodo.propTypes = {
+  dispatch: PropTypes.func.isRequired
+}
 
-export default AddTodo
+export default connect()(AddTodo)
